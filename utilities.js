990031d318270ea207c11b9b9279c947def4c59e
hsiangting49 <157249687+hsiangting49@users.noreply.github.com>
2024-03-04 16:20:56 +0000

    v3

--- utilities.js
+++ utilities.js
@@ -1,14 +1,10 @@
 
-const namelist = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'];
-
 function getRandomName() {
-    return namelist[Math.floor(Math.random() * namelist.length)];
+    const namelist = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'];
+    return {
+        get name() {
+            return namelist[Math.floor(Math.random() * namelist.length)];
+        },
+    }
 }
 
-Alpine.data('namelist', () => ({
-    name: '',
-
-    getname() {
-        this.name = getRandomName();
-    }
-}));
