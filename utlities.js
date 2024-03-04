0b8f72d83a9cb3ad7968da0ac901a2e7b0d80c0b
hsiangting49 <157249687+hsiangting49@users.noreply.github.com>
2024-03-04 04:21:12 +0000

    v2

--- utlities.js
+++ utlities.js
@@ -1,4 +1,4 @@
-const namelist = ['a','b','c', 'd', 'e','f','g','h','i','j'];
+const namelist = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'];
 
 function getRandomName() {
     return namelist[Math.floor(Math.random() * namelist.length)];
